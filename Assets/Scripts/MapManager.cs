5a07f5bad66f545bd9a7f9a3731c2e809e27242e
Takehime <silviapimpaovasquez@hotmail.com>
2017-08-15 15:19:27 -0300

    v0.2.16
    - Adição de um novo tipo de passageiro (TRABALHADOR);
    - Adição da janela de informações sobre as estações do mapa;

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,33 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MapManager : MonoBehaviour {
 
     public GameObject map_window;
+	public GameObject info_window;
     public GameObject station_prefab;
     public GameObject connection_prefab;
     public List<GameObject> routeMap = new List<GameObject>();
+	//public List<StationData> stations;
 
-    private bool window_active = false;
-
-    void Awake() {
-        //routeMap.Add(station_prefab);
-        //for (int i = 0; i < number_of_stations; i++) {
-        //    routeMap.Add(connection_prefab);
-        //    routeMap.Add(station_prefab);
-        //}
-        //Grid.printList(routeMap);
-    }
+    private bool map_window_active = false;
+	private bool info_window_active = false;
 
     public void _openCloseMap() {
         bool aux;
-        if (!window_active) {
+		if (!map_window_active) {
             aux = true;
         } else {
             aux = false;
+			info_window.SetActive (false);
+			info_window_active = false;
         }
-        window_active = aux;
+		map_window_active = aux;
         map_window.SetActive(aux);
     }
 
@@ -37,7 +34,32 @@ public class MapManager : MonoBehaviour {
             routeMap[index - 1].transform.GetChild(0).gameObject.SetActive(false);
         }
         routeMap[index].transform.GetChild(0).gameObject.SetActive(true);
-        print("==> " + routeMap[index].transform.GetChild(0).gameObject.name);
+        //print("==> " + routeMap[index].transform.GetChild(0).gameObject.name);
     }
+
+	public void _showStationInfo(GameObject go) {
+		print ("info_window: " + info_window);
+		if (!info_window_active) {
+			info_window.SetActive (true);
+			info_window_active = true;
+		}
+		StationData st = go.GetComponent<Station> ().sd;
+		setInfoText (st);
+	}
+
+	void setInfoText(StationData st) {
+		//StationData data = info_window.GetComponentInChildren<Station> ().sd;
+		string infoText = "ESTAÇÃO: " + st.name + "\nPERFIS: ";
+		string perfis = "";
+		Grid.printList (st.perfis);
+		for (int i = 0; i < st.perfis.Count; i++) {
+			perfis += st.perfis[i];
+			if (i < st.perfis.Count - 1) {
+				perfis += ", ";
+			}
+		}
+		infoText += perfis.ToLower();
+		info_window.GetComponentInChildren<Text> ().text = infoText;
+	}
 
 }
--- Assets/Scripts/ScriptableObjetcs/PassengerData.cs
+++ Assets/Scripts/ScriptableObjetcs/PassengerData.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum PassengerType {
-    IDOSO, ESTUDANTE, TURISTA, VAREJISTA
+    IDOSO, ESTUDANTE, TURISTA, VAREJISTA, TRABALHADOR
 };
 
 [CreateAssetMenu(menuName = "Custom/Passenger")]
 public class PassengerData : ScriptableObject {
 
-    
     public Color color;
     public PassengerType type;
 }
